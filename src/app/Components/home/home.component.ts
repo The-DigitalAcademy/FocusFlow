30647cbdbaeb9a1fe2fb52ff28d877bb06910a62
V-Ndaba <vusindaba11@gmail.com>
2025-11-06 13:46:44 +0200

    Filter messages and swap between the category function

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, computed, signal } from '@angular/core';
+import { Lists } from 'src/app/models/Lists';
+import { FilterService } from 'src/app/services/filter.service';
+import { ListService } from 'src/app/services/list.service';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +11,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  list = signal<Lists[]>([]);
 
+  //Constructor
+  constructor(
+    private httpClient: HttpClient,
+    private filterService: FilterService,
+    private taskService: TaskService,
+    private listService: ListService
+  ){}
+
+  filteredItems = computed(() => {
+    const filter = this.filterService.selectedFilter();
+    if(!filter) return this.list();
+    return this.list().filter(item => item.category === filter);
+  });
+
+  ngOnInit(): void {
+    this.listService.getAll().subscribe({
+      next: (response: Lists[]) => this.list.set(response),
+      error: (err) => console.log('Error loading lists: ', err)
+    })
+  }
 }
