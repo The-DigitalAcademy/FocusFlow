ea561d95baf938a0569182c3f6e9087abf33b4c4
Thando <thandwem045@gmail.com>
2025-11-10 12:03:41 +0200

    Added way to populate form

--- src/app/Components/add-task/add-task.component.ts
+++ src/app/Components/add-task/add-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Tasks } from 'src/app/models/Tasks';
 import { TaskService } from 'src/app/services/task.service';
 
@@ -8,22 +8,42 @@ import { TaskService } from 'src/app/services/task.service';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent {
-// Has the new task data on it.
+  // Has the new task data on it.
   addedTasks: Tasks[] = [];
-  
-  constructor( 
-      private taskService : TaskService,
-     ) {}
 
-  taskTitle: any = '';
-  taskDueDate: any = '';
-  taskPriority: any = '';
+  // Form fields 
+  taskTitle: any = ''
+  taskDueDate: any = ''
+  taskPriority: any = ''
+
 
   @Input() isVisible: boolean = false;
   @Output() close = new EventEmitter<void>();
   @Input() editTaskId: number | null = null;
-  @Input() addTaskArr: Tasks[] = []
 
+  // Updates the task array for both parent and child
+  @Input() addTaskArr: Tasks[] = []
+  @Output() addTaskArrChange = new EventEmitter<Tasks[]>();
+
+  constructor(
+    private taskService: TaskService,
+  ) { }
+
+  ngOnInit() {
+    if (this.editTaskId !== null) {
+      const taskToEdit = this.addTaskArr.find(t => t.id === this.editTaskId);
+      if (taskToEdit) {
+        this.taskTitle = taskToEdit.name;
+        this.taskDueDate = taskToEdit.dueDate;
+        this.taskPriority = taskToEdit.importance;
+      }
+    } else {
+      // Add task input is empty when clicked 
+      this.taskTitle = '';
+      this.taskDueDate = '';
+      this.taskPriority = '';
+    }
+  }
 
   onClose() {
     this.close.emit();
@@ -31,33 +51,38 @@ export class AddTaskComponent {
   }
 
   addTask() {
-  const newTask = {
-    id: 1, 
-    name: this.taskTitle,
-    dueDate: this.taskDueDate,
-    importance: this.taskPriority,
-    isDone: false
-  };
+
+    const newTask = {
+      id: 1,
+      name: this.taskTitle,
+      dueDate: this.taskDueDate,
+      importance: this.taskPriority,
+      isDone: false
+    };
 
     this.taskService.addTask(newTask).subscribe({
       next: (createdTask) => {
-      this.addTaskArr.push(createdTask);
-      alert('Task Added Successfully!');
-      // Tells the parent to close the model 
-      this.close.emit();
-    
-      console.log(this.addTaskArr);
+        this.addTaskArr.push(createdTask);
+        alert('Task Added Successfully!');
+        this.addTaskArrChange.emit(this.addTaskArr);
+        // Tells the parent to close the model 
+        this.close.emit();
+        console.log(this.addTaskArr);
 
-    },
-    error: (err) => {
-      console.error('Error adding task:', err);
-      alert('Failed to add task.');
-    }
-   })
-    
-}
+      },
+      error: (err) => {
+        console.error('Error adding task:', err);
+        alert('Failed to add task.');
+      }
+    })
 
-showInputValues() {
-  this.editTaskId
-}
+  }
+
+  editTask() {
+     
+  }
+
+  showInputValues() {
+    this.editTaskId
+  }
 }
